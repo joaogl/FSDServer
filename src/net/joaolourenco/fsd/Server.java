9d677600c930679df977d1b4296da76ac3aff457
joaogl <joaoguerralourenco@hotmail.com>
2014-01-26 20:06:12 +0000

    Is now ready to be used.

--- src/net/joaolourenco/fsd/Server.java
+++ src/net/joaolourenco/fsd/Server.java
@@ -17,23 +17,18 @@
 
 package net.joaolourenco.fsd;
 
-import java.io.BufferedInputStream;
-import java.io.DataInputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
+import net.joaolourenco.fsd.data.Clients;
 import net.joaolourenco.fsd.data.ServerLogger;
 import net.joaolourenco.fsd.data.ServerValues;
 
 public class Server implements Runnable {
 
 	private ServerSocket server = null;
-	private ServerClient clients[];
 	private Thread thread = null;
-	private static int clientCount = 0;
-
-	//private IncommingHandler inhandler = new IncommingHandler();
 
 	/**
 	 * This is where the main socket is created
@@ -54,7 +49,7 @@ public class Server implements Runnable {
 			ServerLogger.println("Can not bind to port " + port + ": " + ioe.getMessage());
 		}
 		// We are now creating a array of clients with the size from the config file
-		clients = new ServerClient[ServerValues.Max_Clients.getPropertie()];
+		Clients.setupClients(ServerValues.Max_Clients.getPropertie());
 	}
 
 	/**
@@ -118,91 +113,23 @@ public class Server implements Runnable {
 	 */
 	private void addThread(Socket socket) {
 		boolean accepted = false;
-		if (clientCount < clients.length) {
-			String[] name = new String[3];
-			try {
-				DataInputStream streamIn = new DataInputStream(new BufferedInputStream(socket.getInputStream()));
-				String inputLine;
-
-				while ((inputLine = streamIn.readUTF()) != null) {
-					if (inputLine.contains("newcom ") || inputLine.contains("newmancom ")) {
-						name = inputLine.split(" ");
-						if (name.length >= 3) {
-							if (name[1] != null && name[2] != null) {
-								if (inputLine.contains("newcom ")) ProgramInfo.connectedPilots[socket.getPort()] = name[1];
-								else if (inputLine.contains("newmancom ")) ProgramInfo.connectedManagers[socket.getPort()] = name[1];
-								accepted = true;
-								break;
-							}
-						} else {
-							ServerLogger.println("Hacked client trying to join: " + socket.getInetAddress());
-							if (socket != null) socket.close();
-							if (streamIn != null) streamIn.close();
-						}
-					}
-				}
-			} catch (IOException e) {
-				if (e.getMessage().contains("Stream closed")) ServerLogger.println("Hacked client connection ended.");
-				else e.printStackTrace();
-			}
+		if (Clients.availableSlots()) {
+			// In the future there will be some code to check if the client is valid.
+			accepted = true;
 
 			if (accepted) {
-				if (ProgramInfo.connectedPilots[socket.getPort()] == name[1]) ServerLogger.println("Client " + socket.getInetAddress() + " as connected with the PilotID " + name[1]);
-				else ServerLogger.println("Manager " + socket.getInetAddress() + " as connected with the name " + name[1]);
-				try {
-					clients[clientCount] = new ServerThread(this, socket, name[2], name[1]);
-					clients[clientCount].open();
-					clients[clientCount].start();
-					clientCount++;
-				} catch (IOException e) {
-					ServerLogger.println("Error opening thread: " + e);
-				}
+				// Printing the information that a client has connected.
+				ServerLogger.println("Client " + socket.getInetAddress() + " as connected.");
+				// Adding the client to the connected clients list.
+				Clients.addClient(socket, this);
 			}
-			accepted = false;
-		} else ServerLogger.println("Client refused: maximum " + clients.length + " reached.");
-	}
-
-	private static int findClient(int ID) {
-		for (int i = 0; i < clientCount; i++)
-			if (clients[i].getID() == ID) return i;
-		return -1;
-	}
-
-	public synchronized void handle(int ID, String input) {
-		if (input.equals("disc")) {
-			clients[findClient(ID)].send("disc");
-			remove(ID);
 		} else {
-			inhandler.processInput(input, ID);
-			if (ProgramInfo.connectedPilots[ID] != null) clients[findClient(ID)].send(ProgramInfo.connectedPilots[ID] + ": " + input);
-			else clients[findClient(ID)].send(ProgramInfo.connectedManagers[ID] + ": " + input);
-		}
-	}
-
-	public synchronized void handleForAll(int ID, String input) {
-		if (input.equals("disc")) {
-			clients[findClient(ID)].send("disc");
-			remove(ID);
-		} else for (int i = 0; i < clientCount; i++) {
-			if (ProgramInfo.connectedPilots[ID] != null) clients[i].send(ProgramInfo.connectedPilots[ID] + ": " + input);
-			else clients[i].send(ProgramInfo.connectedManagers[ID] + ": " + input);
-		}
-	}
-
-	public static synchronized void remove(int ID) {
-		int pos = findClient(ID);
-		if (pos >= 0) {
-			ServerThread toTerminate = clients[pos];
-			if (ProgramInfo.connectedPilots[ID] != null) ServerLogger.println("Removing client " + ProgramInfo.connectedPilots[ID] + ".");
-			else ServerLogger.println("Removing Manager " + ProgramInfo.connectedManagers[ID] + ".");
-			if (pos < clientCount - 1) for (int i = pos + 1; i < clientCount; i++)
-				clients[i - 1] = clients[i];
-			clientCount--;
+			ServerLogger.println("Client refused: maximum " + Clients.getConnectedClients() + " reached.");
+			// Closing the socket.
 			try {
-				toTerminate.close();
-				toTerminate.stop();
-			} catch (IOException ioe) {
-				ServerLogger.println("Error closing thread: " + ioe);
+				socket.close();
+			} catch (IOException e) {
+				e.printStackTrace();
 			}
 		}
 	}
