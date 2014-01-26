c0f47924b90cbe0810bcfb79dd9695e55bb91d91
joaogl <joaoguerralourenco@hotmail.com>
2014-01-26 21:48:11 +0000

    Finished the ServerClient Class.

--- src/net/joaolourenco/fsd/ServerClient.java
+++ src/net/joaolourenco/fsd/ServerClient.java
@@ -22,73 +22,250 @@ import java.io.BufferedOutputStream;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.net.InetAddress;
 import java.net.Socket;
 
+import net.joaolourenco.fsd.data.Clients;
 import net.joaolourenco.fsd.data.ServerLogger;
+import net.joaolourenco.fsd.data.sockets.InHandler;
 
-public class ServerClient extends Thread {
-	private Server server = null;
-	private Socket socket = null;
-	private String pw = null;
-	private int ID = -1;
+public class ServerClient implements Runnable {
+
+	/**
+	 * This is a variable to keep the client loop running.
+	 * 
+	 * @author João Lourenço
+	 * @category Variables
+	 */
+	private boolean running = false;
+
+	/**
+	 * This is the thread that is going to be used for each client.
+	 * 
+	 * @author João Lourenço
+	 * @category Variables
+	 */
+	private Thread thread;
+
+	/**
+	 * This is the socket used to communicate to one client.
+	 * 
+	 * @author João Lourenço
+	 * @category Variables
+	 */
+	private final Socket socket;
+
+	/**
+	 * This is where the password will be kept for login purposes.
+	 * 
+	 * @author João Lourenço
+	 * @category Variables
+	 */
+	private final String pw;
+
+	/**
+	 * This is the ID in the clients class.
+	 * 
+	 * @author João Lourenço
+	 * @category Variables
+	 */
+	private final int ID;
+
+	/**
+	 * This is the name of the client.
+	 * 
+	 * @author João Lourenço
+	 * @category Variables
+	 */
+	private final String Name;
+
+	/**
+	 * This is the stream where communication will came from.
+	 * 
+	 * @author João Lourenço
+	 * @category Variables
+	 */
 	private DataInputStream streamIn = null;
+
+	/**
+	 * This is the stream where communication will go to.
+	 * 
+	 * @author João Lourenço
+	 * @category Variables
+	 */
 	private DataOutputStream streamOut = null;
 
-	public ServerClient(Server _server, Socket _socket, String _pw, String _id) throws IOException {
-		super();
-		server = _server;
-		socket = _socket;
-		pw = _pw;
-		ID = socket.getPort();
-		if (!DataManager.getPilot(_id, pw)) {
-			close();
-			server.remove(ID);
-			stop();
+	/**
+	 * This is where we create the new Client.
+	 * 
+	 * @author João Lourenço
+	 * @category Constructors
+	 * @param _socket
+	 *            The client socket.
+	 * @param _pw
+	 *            The client password.
+	 * @param _cID
+	 *            The client name.
+	 * @param _rID
+	 *            The client ID.
+	 */
+	public ServerClient(Socket _socket, String _pw, String _cID, int _rID) {
+		// We are setting the local variables to be the same as the parameters. 
+		this.socket = _socket;
+		this.pw = _pw;
+		this.ID = _rID;
+		this.Name = _cID;
+		// We are checking for a login.
+		if (!Clients.checkLogin(this.Name, this.pw)) {
+			// Because the login wasn't valid lets close and remove the client.
+			Clients.removeClient(this.ID);
+		} else this.start();
+	}
+
+	/**
+	 * This is where the client thread is created and started as Client Thread.
+	 * 
+	 * @author João Lourenço
+	 * @category Threads
+	 */
+	public synchronized void start() {
+		if (this.thread == null) {
+			// Starting the variable used to keep the client loop running.
+			this.running = true;
+			// Creating a new thread if it's not created yet.
+			this.thread = new Thread(this, "Client Thread");
+			// Starting the thread.
+			this.thread.start();
 		}
 	}
 
-	public void send(String msg) {
-		try {
-			ServerLogger.println(msg);
-			streamOut.writeUTF(msg);
-			streamOut.flush();
-		} catch (IOException ioe) {
-			ServerLogger.println(ID + " ERROR sending: " + ioe.getMessage());
-			server.remove(ID);
-			stop();
-		}
-	}
-
-	public int getID() {
-		return ID;
-	}
-
-	public void run() {
-		while (true) {
+	/**
+	 * This is where the client thread is stopped.
+	 * 
+	 * @author João Lourenço
+	 * @category Threads
+	 */
+	public synchronized void stop() {
+		// Stopping the Client loop
+		this.running = false;
+		if (this.thread != null) {
 			try {
-				server.handle(ID, streamIn.readUTF());
-			} catch (IOException ioe) {
-				ServerLogger.println(ID + " ERROR reading: " + ioe.getMessage());
-				server.remove(ID);
-				try {
-					close();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-				stop();
+				// Making the thread to stop if there is a thread.
+				this.thread.join();
+			} catch (InterruptedException e) {
+				// Printing an error if soemthing goes wrong.
+				e.printStackTrace();
 			}
 		}
 	}
 
-	public void open() throws IOException {
-		streamIn = new DataInputStream(new BufferedInputStream(socket.getInputStream()));
-		streamOut = new DataOutputStream(new BufferedOutputStream(socket.getOutputStream()));
+	/**
+	 * This is where the client loop is running.
+	 * 
+	 * @author João Lourenço
+	 * @category Threads
+	 */
+	public void run() {
+		while (this.running) {
+			try {
+				// Creating a new Handler for the input
+				new InHandler(this.ID, this.streamIn.readUTF());
+			} catch (IOException ioe) {
+				// Printing and removing the client in case something goes wrong.
+				ServerLogger.println(this.ID + " ERROR reading: " + ioe.getMessage());
+				Clients.removeClient(this.ID);
+			}
+		}
 	}
 
-	public void close() throws IOException {
-		if (socket != null) socket.close();
-		if (streamIn != null) streamIn.close();
-		if (streamOut != null) streamOut.close();
+	/**
+	 * This is used to send a message to the client.
+	 * 
+	 * @author João Lourenço
+	 * @category Clients
+	 * @param msg
+	 *            Message we want to send as a String.
+	 */
+	public void send(String msg) {
+		try {
+			// Printing the message and sending through the stream.
+			ServerLogger.println(msg);
+			this.streamOut.writeUTF(msg);
+			this.streamOut.flush();
+		} catch (IOException ioe) {
+			// Printing and removing the client in case something goes wrong.
+			ServerLogger.println(this.ID + " ERROR sending: " + ioe.getMessage());
+			Clients.removeClient(this.ID);
+		}
+	}
+
+	/**
+	 * This is used to get te client ID.
+	 * 
+	 * @author João Lourenço
+	 * @category Clients
+	 * @return ID as an integer.
+	 */
+	public int getID() {
+		return this.ID;
+	}
+
+	/**
+	 * This is used to get te client Name.
+	 * 
+	 * @author João Lourenço
+	 * @category Clients
+	 * @return Name as a String.
+	 */
+	public String getName() {
+		return this.Name;
+	}
+
+	/**
+	 * This is used to get te client Address.
+	 * 
+	 * @author João Lourenço
+	 * @category Clients
+	 * @return Address as an InetAddress.
+	 */
+	public InetAddress getAddress() {
+		return this.socket.getInetAddress();
+	}
+
+	/**
+	 * This is used to open the streams.
+	 * 
+	 * @author João Lourenço
+	 * @category Clients
+	 */
+	public void open() {
+		try {
+			// Creating the client streams.
+			this.streamIn = new DataInputStream(new BufferedInputStream(this.socket.getInputStream()));
+			this.streamOut = new DataOutputStream(new BufferedOutputStream(this.socket.getOutputStream()));
+		} catch (IOException e) {
+			// Printing and removing the client in case something goes wrong.
+			ServerLogger.println(this.ID + " ERROR opening streams: " + e.getMessage());
+			Clients.removeClient(this.ID);
+		}
+	}
+
+	/**
+	 * This is used to close the sockets.
+	 * 
+	 * @author João Lourenço
+	 * @category Clients
+	 */
+	public void close() {
+		try {
+			// Closing the sockets if they haven't been closed already.
+			if (socket != null) socket.close();
+			if (streamIn != null) streamIn.close();
+			if (streamOut != null) streamOut.close();
+		} catch (IOException e) {
+			// Printing the error.
+			ServerLogger.println(this.ID + " ERROR closing streams: " + e.getMessage());
+		}
 	}
 
 }
